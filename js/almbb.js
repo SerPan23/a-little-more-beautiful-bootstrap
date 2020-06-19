f995da0062d2d5a5d473580c12cc200eb3991bb0
SerPan23 <igracom@yandex.ru>
2020-06-19 20:51:50 +0300

    alpha_v_0_0_2

--- js/almbb.js
+++ js/almbb.js
@@ -1,11 +1,12 @@
-//animated-bootstrap-hamburger
-$('.navbar-collapse').on('click', function () {
-    $('.navbar-collapse').collapse('hide');
-    $('.navbar-toggler').removeClass("active");
-});
 $(document).ready(function () {
+    //animated-bootstrap-hamburger
+    $('.navbar-collapse').on('click', function () {
+        $('.navbar-collapse').collapse('hide');
+        $('.navbar-toggler').removeClass("active");
+    });
+
     $(".navbar-toggler").on("click", function () {
         $(this).toggleClass("active");
     });
+    //-------
 });
-//-------
